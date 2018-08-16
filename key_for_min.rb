aa41fd26f4eb7e3c6d4731f0a292e91af2bac0b1
Jonathan Chan <johnkchan252@gmail.com>
2018-08-16 00:25:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.empty?
+    NIL
+  end
+  
   min = 0
   lowest = ""
   set = false
